5456e9c978c57dc12ecd43dbcfdb00223ee161f0
Dave the Pear <86448428+davethepear@users.noreply.github.com>
2021-06-24 23:04:40 -0500

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,1 +1,2 @@
 echo "$(tput setaf 2)CPU1:$(tput setaf 3) `sensors -f | grep Core\ 0 | awk '{ print $3 }'` $(tput setaf 2)CPU2:$(tput setaf 3) `sensors -f | grep Core\ 1 | awk '{ print $3 }'` $(tput setaf 2)"
+# after you install and setup lm-sensors, you can check to see what your cpu is called. mine was "Core 0", this monitors both my CPUs, modify this to your needs.
