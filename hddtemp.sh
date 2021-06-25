9dd38d9dd5584a9e7f56b6ad6601bd1a356bfbb9
Dave the Pear <86448428+davethepear@users.noreply.github.com>
2021-06-24 22:58:29 -0500

    Update hddtemp.sh

--- hddtemp.sh
+++ hddtemp.sh
@@ -1,2 +1,3 @@
 echo "System Drive: $(tput setaf 3)`sudo hddtemp -n -uF /dev/sda`°F $(tput setaf 2) Hard Drive 1: $(tput setaf 3)`sudo hddtemp -n -uF /dev/sdb`°F $(tput setaf 2)"
 echo "                     Hard Drive 2: $(tput setaf 3)`sudo hddtemp -n -uF /dev/sdc`°F $(tput setaf 2) Hard Drive 3: $(tput setaf 3)`sudo hddtemp -n -uF /dev/sdd`°F $(tput setaf 2)"
+# I have 4 drives here that I'm monitoring. Add or remove as needed. Change them to reflect your setup.
